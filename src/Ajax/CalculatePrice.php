1ac837e28510867cbff33c003421b3bd66a00584
EdgarkFS <khachaturovedgar01@gmail.com>
2026-06-10 12:13:08 +0400

    CalculatePrice: return sale strikethrough HTML for live configurator update

--- src/Ajax/CalculatePrice.php
+++ src/Ajax/CalculatePrice.php
@@ -40,8 +40,10 @@ class CalculatePrice {
 			wp_send_json_error( [ 'message' => __( 'No price blueprint assigned.', 'priceblueprint-for-woocommerce' ) ] );
 		}
 
-		$rules = RulesCache::get( $template_id );
-		$total = (float) get_post_meta( $product_id, '_price', true );
+		$rules     = RulesCache::get( $template_id );
+		$base_sale = (float) get_post_meta( $product_id, '_price',         true );
+		$base_reg  = (float) get_post_meta( $product_id, '_regular_price', true );
+		$additions = 0.0;
 
 		foreach ( $selections as $attribute => $value_slug ) {
 			$attribute  = sanitize_key( $attribute );
@@ -49,16 +51,21 @@ class CalculatePrice {
 
 			foreach ( $rules as $rule ) {
 				if ( $rule['attribute'] === $attribute && in_array( $value_slug, $rule['value_slugs'] ?? [], true ) ) {
-					$total += (float) $rule['price'];
+					$additions += (float) $rule['price'];
 					break;
 				}
 			}
 		}
 
-		$quantity = max( 1, absint( $_POST['quantity'] ?? 1 ) );
+		$quantity      = max( 1, absint( $_POST['quantity'] ?? 1 ) );
+		$sale_total    = ( $base_sale + $additions ) * $quantity;
+		$regular_total = ( $base_reg  + $additions ) * $quantity;
+		$product       = wc_get_product( $product_id );
 
-		wp_send_json_success( [
-			'formatted' => wp_strip_all_tags( wc_price( $total * $quantity ) ),
-		] );
+		$formatted = ( $product && $product->is_on_sale() )
+			? wc_format_sale_price( wc_price( $regular_total ), wc_price( $sale_total ) )
+			: wc_price( $sale_total );
+
+		wp_send_json_success( [ 'formatted' => $formatted ] );
 	}
 }
